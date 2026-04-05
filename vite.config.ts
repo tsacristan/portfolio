5b81f9310384b3dbc68a5a1f55d68317b5ae3215
Thurian <thurian.sacristan@etu.unilim.fr>
2026-04-05 21:20:44 +0200

    Build optimisé pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Portfolio/',
+  base: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
